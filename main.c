2bafda3daa96cca9bf4bd2ba2c6bd1be12df0c6d
alumno <alumno>
2018-08-27 11:33:47 -0300

    primer commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numeroUno;
+    int numeroDos;
+    int numeroTres;
+
+    printf("ingrese el primer numero \n");
+    scanf("%d",&numeroUno);
+    printf("ingrese el segundo numero \n");
+    scanf("%d",&numeroDos);
+    printf("ingrese el tercer numero \n");
+    scanf("%d",&numeroTres);
+
+    printf("%d\n",numeroUno);
+    printf("%d\n",numeroDos);
+    printf("%d\n",numeroTres);
+    return 0;
+}
